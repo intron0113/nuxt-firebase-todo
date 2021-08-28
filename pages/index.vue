922e11709ea02eab8e17395791f87d2d53d208fe
intron0113 <83868900+intron0113@users.noreply.github.com>
2021-08-29 07:05:35 +0900

    nuxt-firebase-todo

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>Hello, Vuetify!</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
